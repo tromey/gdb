44aa9882cbd668fdbf1f377825099498a5d867f8
Tom Tromey <tromey@redhat.com>
2014-03-17 08:29:16 -0600

    add gccjit-internal.h
    This adds gccjit-internal.h and moves some declarations into it.

---
+++ gdb/gccjit/gccjit-internal.h
@@ -0,0 +1,52 @@
+/* Header file for GDB GCC JIT.
+   Copyright (C) 2014 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#ifndef GDB_GCCJIT_GCCJIT_INTERNAL_H
+#define GDB_GCCJIT_GCCJIT_INTERNAL_H
+
+#include "hashtab.h"
+#include "gcc-interface.h"
+
+/* An object of this type holds state associated with a given
+   compilation job.  */
+
+struct gdb_gcc_instance
+{
+  /* The GCC front end.  */
+
+  struct gcc_context *fe;
+
+  /* Map from gdb types to gcc types.  */
+
+  htab_t type_map;
+};
+
+/* Convert a gdb type, TYPE, to a GCC type.  CONTEXT is used to do the
+   actual conversion.  The new GCC type is returned.  */
+
+extern gcc_type convert_type (struct gdb_gcc_instance *context,
+			      struct type *type);
+
+/* Instantiate a GDB object holding state for the GCC context FE.  THe
+   new object is returned.  */
+
+extern struct gdb_gcc_instance *new_gdb_gcc_instance (struct gcc_context *fe);
+
+/* Delete an object created by new_gdb_gcc_instance.  */
+
+extern void delete_gdb_gcc_instance (struct gdb_gcc_instance *context);
+
+#endif /* GDB_GCCJIT_GCCJIT_INTERNAL_H */
