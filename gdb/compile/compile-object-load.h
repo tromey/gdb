0df088ff3f450ee0b3d6df1ed365203bf5a76e6f
Jan Kratochvil <jan.kratochvil@redhat.com>
2015-05-13 21:03:49 +0200

    compile: New 'compile print'
    It is planned the existing GDB command 'print' will be able to evaluate its
    expressions using the compiler.  There will be some option to choose between
    the existing GDB evaluation and the compiler evaluation.  But as an
    intermediate step this patch provides the expression printing feature as a new
    command.
    
    I can imagine it could be also called 'maintenance compile print' as in the
    future one should be able to use its functionality by the normal 'print'
    command.
    
    There was a discussion with Eli about the command name:
    	https://sourceware.org/ml/gdb-patches/2015-03/msg00880.html
    As there were no other comments yet I haven't renamed it yet, before there is
    some confirmation about settlement on the final name.
    
    Support for the GDB '@' operator to create arrays has been submitted for GCC:
    	[gcc patch] libcc1: '@' GDB array operator
    	https://gcc.gnu.org/ml/gcc-patches/2015-03/msg01451.html
    
    
    gdb/ChangeLog
    2015-03-26  Jan Kratochvil  <jan.kratochvil@redhat.com>
    	    Phil Muldoon  <pmuldoon@redhat.com>
    
    	* NEWS (Changes since GDB 7.9): Add compile print.
    	* compile/compile-c-support.c (add_code_header, add_code_footer)
    	(c_compute_program): Add COMPILE_I_PRINT_ADDRESS_SCOPE and
    	COMPILE_I_PRINT_VALUE_SCOPE.
    	* compile/compile-internal.h (COMPILE_I_PRINT_OUT_ARG_TYPE)
    	(COMPILE_I_PRINT_OUT_ARG, COMPILE_I_EXPR_VAL, COMPILE_I_EXPR_PTR_TYPE):
    	New.
    	* compile/compile-object-load.c: Include block.h.
    	(get_out_value_type): New function.
    	(compile_object_load): Handle COMPILE_I_PRINT_ADDRESS_SCOPE and
    	COMPILE_I_PRINT_VALUE_SCOPE.  Set compile_module's OUT_VALUE_ADDR and
    	OUT_VALUE_TYPE.
    	* compile/compile-object-load.h (struct compile_module): Add fields
    	out_value_addr and out_value_type.
    	* compile/compile-object-run.c: Include valprint.h and compile.h.
    	(struct do_module_cleanup): Add fields out_value_addr and
    	out_value_type.
    	(do_module_cleanup): Handle COMPILE_I_PRINT_ADDRESS_SCOPE and
    	COMPILE_I_PRINT_VALUE_SCOPE.
    	(compile_object_run): Propagate out_value_addr and out_value_type.
    	Pass OUT_VALUE_ADDR.
    	* compile/compile.c: Include valprint.h.
    	(compile_print_value, compile_print_command): New functions.
    	(eval_compile_command): Handle failed COMPILE_I_PRINT_ADDRESS_SCOPE.
    	(_initialize_compile): Update compile code help text.  Install
    	compile_print_command.
    	* compile/compile.h (compile_print_value): New prototype.
    	* defs.h (enum compile_i_scope_types): Add
    	COMPILE_I_PRINT_ADDRESS_SCOPE and COMPILE_I_PRINT_VALUE_SCOPE.
    
    gdb/doc/ChangeLog
    2015-03-26  Jan Kratochvil  <jan.kratochvil@redhat.com>
    
    	* gdb.texinfo (Compiling and Injecting Code): Add compile print.
    
    gdb/testsuite/ChangeLog
    2015-03-26  Jan Kratochvil  <jan.kratochvil@redhat.com>
    
    	* gdb.compile/compile-print.c: New file.
    	* gdb.compile/compile-print.exp: New file.

--- gdb/compile/compile-object-load.h
+++ gdb/compile/compile-object-load.h
@@ -37,6 +37,14 @@ struct compile_module
 
   /* User data for SCOPE in use.  */
   void *scope_data;
+
+  /* Inferior parameter out value address or NULL if the inferior function does not
+     have one.  */
+  CORE_ADDR out_value_addr;
+
+  /* Inferior parameter out value type or NULL if the inferior function does not
+     have one.  */
+  struct type *out_value_type;
 };
 
 extern struct compile_module *compile_object_load
