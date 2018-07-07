92b009b91a0d10597f282eb005b1f6c915adfb8f
Tom Tromey <tom@tromey.com>
2018-07-06 21:38:46 -0600

    colorizing

--- gdb/silent-rules.mk
+++ gdb/silent-rules.mk
@@ -1,15 +1,35 @@
 # If V is undefined or V=0 is specified, use the silent/verbose/compact mode.
 V ?= 0
 ifeq ($(V),0)
-ECHO_CXX =    @echo "  CXX    $@";
-ECHO_CXXLD =  @echo "  CXXLD  $@";
-ECHO_REGDAT = @echo "  REGDAT $@";
-ECHO_GEN =    @echo "  GEN    $@";
+
+.DEFAULT_GOAL := all
+
+set-color.mk: .
+	@if test -t 1; then \
+	  echo COLORIZE=1 > set-color.mk; \
+	else \
+	  echo COLORIZE=0 > set-color.mk; \
+	fi
+
+.PHONY: set-color.mk
+
+include set-color.mk
+
+ifeq ($(COLORIZE),1)
+colorize = \033[32m$(1)\033[0m
+else
+colorize = $(1)
+endif
+
+ECHO_CXX =    @echo -e "  $(call colorize,CXX)    $@";
+ECHO_CXXLD =  @echo -e "  $(call colorize,CXXLD)  $@";
+ECHO_REGDAT = @echo -e "  $(call colorize,REGDAT) $@";
+ECHO_GEN =    @echo -e "  $(call colorize,GEN)    $@";
 ECHO_GEN_XML_BUILTIN = \
-              @echo "  GEN    xml-builtin.c";
+              @echo -e "  $(call colorize,GEN)    xml-builtin.c";
 ECHO_GEN_XML_BUILTIN_GENERATED = \
-              @echo "  GEN    xml-builtin-generated.c";
-ECHO_INIT_C =  echo "  GEN    init.c" ||
-ECHO_SIGN =   @echo "  SIGN   gdb";
+              @echo -e "  $(call colorize,GEN)    xml-builtin-generated.c";
+ECHO_INIT_C =  echo -e "  $(call colorize,GEN)    init.c" ||
+ECHO_SIGN =   @echo -e "  $(call colorize,SIGN)   gdb";
 SILENCE = @
 endif
