1813558c53e7f375fabe39cfe0f867d169ba580f
Tom Tromey <tom@tromey.com>
2019-01-03 22:21:41 -0700

    class-ize psymtabs a bit

--- gdb/stabsread.h
+++ gdb/stabsread.h
@@ -170,8 +170,8 @@ extern void finish_global_stabs (struct objfile *objfile);
 /* Functions exported by dbxread.c.  These are not in stabsread.c because
    they are only used by some stabs readers.  */
 
-extern struct partial_symtab *dbx_end_psymtab
-  (struct objfile *objfile, struct partial_symtab *pst,
+extern struct legacy_partial_symtab *dbx_end_psymtab
+  (struct objfile *objfile, struct legacy_partial_symtab *pst,
    const char **include_list, int num_includes,
    int capping_symbol_offset, CORE_ADDR capping_text,
    struct partial_symtab **dependency_list, int number_dependencies,
