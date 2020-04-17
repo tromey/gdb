562b289768269048e3ba13fe93deab216df9c49c
Tom Tromey <tromey@adacore.com>
2020-04-17 15:13:03 -0600

    this is what we want, see pr 25837

--- gdb/language.c
+++ gdb/language.c
@@ -740,9 +740,9 @@ get_symbol_name_matcher (const language_defn *lang,
   /* If currently in Ada mode, and the lookup name is wrapped in
      '<...>', hijack all symbol name comparisons using the Ada
      matcher, which handles the verbatim matching.  */
-  if (current_language->la_language == language_ada
-      && lookup_name.ada ().verbatim_p ())
-    return current_language->la_get_symbol_name_matcher (lookup_name);
+  /* if (current_language->la_language == language_ada */
+  /*     && lookup_name.ada ().verbatim_p ()) */
+  /*   return current_language->la_get_symbol_name_matcher (lookup_name); */
 
   if (lang->la_get_symbol_name_matcher != nullptr)
     return lang->la_get_symbol_name_matcher (lookup_name);
