3f5ca5eabbf637eb3e8d1118398d4c6b8b157998
Tom Tromey <tom@tromey.com>
2020-07-27 18:42:28 -0600

    hackery

--- sim/common/sim-target.cc
+++ sim/common/sim-target.cc
@@ -1,4 +1,6 @@
 #include "gdbserver/target.h"
+#include "gdb/callback.h"
+#include "gdb/remote-sim.h"
 
 class sim_target : public process_stratum_target
 {
@@ -15,6 +17,13 @@ class sim_target : public process_stratum_target
     return -1;
   }
 
+  int kill (process_info *proc) override
+  {
+    sim_close (m_sim, 1);
+    m_sim = nullptr;
+    return 0;
+  }
+
   int detach (process_info *proc) override
   {
     return -1;
@@ -22,7 +31,11 @@ class sim_target : public process_stratum_target
 
   void mourn (process_info *proc) override
   {
-    sim_close (m_sim, fixme);
+  }
+
+  void mourn (int pid) override
+  {
+    sim_close (m_sim, 0);
     m_sim = nullptr;
   }
 
@@ -33,7 +46,7 @@ class sim_target : public process_stratum_target
 
   void resume (thread_resume *resume_info, size_t n) override
   {
-    sim_resume (m_sim, fixme);
+    // fixme sim_resume (m_sim, fixme);
   }
 
   int handle_monitor_command (char *mon) override
@@ -58,3 +71,21 @@ class sim_target : public process_stratum_target
 private:
   SIM_DESC m_sim;		// FIXME??
 };
+
+void
+blah_blah ()
+{
+      gdb_callback = default_callback;
+      gdb_callback.init (&gdb_callback);
+      gdb_callback.write_stdout = gdb_os_write_stdout;
+      gdb_callback.flush_stdout = gdb_os_flush_stdout;
+      gdb_callback.write_stderr = gdb_os_write_stderr;
+      gdb_callback.flush_stderr = gdb_os_flush_stderr;
+      gdb_callback.printf_filtered = gdb_os_printf_filtered;
+      gdb_callback.vprintf_filtered = gdb_os_vprintf_filtered;
+      gdb_callback.evprintf_filtered = gdb_os_evprintf_filtered;
+      gdb_callback.error = gdb_os_error;
+      gdb_callback.poll_quit = gdb_os_poll_quit;
+      gdb_callback.magic = HOST_CALLBACK_MAGIC;
+      callbacks_initialized = 1;
+    }
