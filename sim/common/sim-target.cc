5c0b97af512b51d5396f31d5d4d0b2b3d649bdfe
Tom Tromey <tom@tromey.com>
2021-02-14 10:01:39 -0700

    more hacking

--- sim/common/sim-target.cc
+++ sim/common/sim-target.cc
@@ -31,23 +31,39 @@ class sim_target : public process_stratum_target
 
   void mourn (process_info *proc) override
   {
-  }
-
-  void mourn (int pid) override
-  {
     sim_close (m_sim, 0);
     m_sim = nullptr;
+
+    remove_process (proc);
   }
 
-  void request_interrupt () const
+  void join (int pid) override
   {
-    sim_stop (m_sim);
   }
+
+  bool thread_alive (ptid_t pid) override;
 
   void resume (thread_resume *resume_info, size_t n) override
   {
-    // fixme sim_resume (m_sim, fixme);
+    gdb_assert (n == 1);
+    m_resume = resume_info[0];
   }
+
+  ptid_t wait (ptid_t ptid, target_waitstatus *status,
+	       target_wait_flags options) override;
+  {
+    if (m_resume.kind == resume_stop)
+      sim_stop (m_sim);
+    else
+      sim_resume (m_sim, m_resume.kind == resume_step, m_resume.sig);
+    // FIXME options?
+    // FIXME *status = ;
+    return ptid;
+  }
+
+  void fetch_registers (regcache *regcache, int regno) override;
+
+  void store_registers (regcache *regcache, int regno) override;
 
   int handle_monitor_command (char *mon) override
   {
@@ -57,35 +73,47 @@ class sim_target : public process_stratum_target
 
   int read_memory (CORE_ADDR memaddr, unsigned char *myaddr, int len) override
   {
-    // fixme return value
-    return sim_read (m_sim, memaddr, myaddr, len);
+    if (sim_read (m_sim, memaddr, myaddr, len) == len)
+      return 0;
+    return 1;
   }
 
   int write_memory (CORE_ADDR memaddr, const unsigned char *myaddr, int len)
     override
   {
-    // fixme return value
-    return sim_write (m_sim, mem, myaddr, len);
+    if (sim_write (m_sim, mem, myaddr, len) == len)
+      return 0;
+    return 1;
+  }
+
+  void request_interrupt () const
+  {
+    sim_stop (m_sim);
   }
 
 private:
-  SIM_DESC m_sim;		// FIXME??
+
+  /* Store the most recent resume request until the wait method is
+     called.  */
+  thread_resume m_resume;
+
+  SIM_DESC m_sim = nullptr;
 };
 
 void
-blah_blah ()
+initialize_sim ()
 {
-      gdb_callback = default_callback;
-      gdb_callback.init (&gdb_callback);
-      gdb_callback.write_stdout = gdb_os_write_stdout;
-      gdb_callback.flush_stdout = gdb_os_flush_stdout;
-      gdb_callback.write_stderr = gdb_os_write_stderr;
-      gdb_callback.flush_stderr = gdb_os_flush_stderr;
-      gdb_callback.printf_filtered = gdb_os_printf_filtered;
-      gdb_callback.vprintf_filtered = gdb_os_vprintf_filtered;
-      gdb_callback.evprintf_filtered = gdb_os_evprintf_filtered;
-      gdb_callback.error = gdb_os_error;
-      gdb_callback.poll_quit = gdb_os_poll_quit;
-      gdb_callback.magic = HOST_CALLBACK_MAGIC;
-      callbacks_initialized = 1;
-    }
+  gdb_callback = default_callback;
+  gdb_callback.init (&gdb_callback);
+  gdb_callback.write_stdout = gdb_os_write_stdout;
+  gdb_callback.flush_stdout = gdb_os_flush_stdout;
+  gdb_callback.write_stderr = gdb_os_write_stderr;
+  gdb_callback.flush_stderr = gdb_os_flush_stderr;
+  gdb_callback.printf_filtered = gdb_os_printf_filtered;
+  gdb_callback.vprintf_filtered = gdb_os_vprintf_filtered;
+  gdb_callback.evprintf_filtered = gdb_os_evprintf_filtered;
+  gdb_callback.error = gdb_os_error;
+  gdb_callback.poll_quit = gdb_os_poll_quit;
+  gdb_callback.magic = HOST_CALLBACK_MAGIC;
+  callbacks_initialized = 1;
+}
