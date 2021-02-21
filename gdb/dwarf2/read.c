1763d123e601bcba5f15aebf2dfdf497d1e28e63
Tom Tromey <tom@tromey.com>
2021-02-20 19:41:14 -0700

    Avoid crash on missing dwz file
    If DWARF contains a reference to a "dwz" file, but there is no
    .gnu_debugaltlink section, then gdb will crash.  This happens because
    dwarf2_get_dwz_file will return NULL, but some callers do not expect
    this.
    
    This patch changes dwarf2_get_dwz_file so that callers can require a
    dwz file.  Then, it updates the callers that are attempting to process
    references to the dwz file to require one.
    
    gdb/ChangeLog
    2021-02-20  Tom Tromey  <tom@tromey.com>
    
    	* dwarf2/read.h (dwarf2_get_dwz_file): Add 'require' parameter.
    	* dwarf2/read.c (dwarf2_get_dwz_file): Add 'require' parameter.
    	(get_abbrev_section_for_cu, read_attribute_value)
    	(get_debug_line_section): Update.
    	* dwarf2/macro.c (dwarf_decode_macro_bytes): Update.

--- gdb/dwarf2/read.c
+++ gdb/dwarf2/read.c
@@ -2279,7 +2279,7 @@ dwz_search_other_debugdirs (std::string &filename, bfd_byte *buildid,
 /* See dwarf2read.h.  */
 
 struct dwz_file *
-dwarf2_get_dwz_file (dwarf2_per_bfd *per_bfd)
+dwarf2_get_dwz_file (dwarf2_per_bfd *per_bfd, bool require)
 {
   bfd_size_type buildid_len_arg;
   size_t buildid_len;
@@ -2295,7 +2295,11 @@ dwarf2_get_dwz_file (dwarf2_per_bfd *per_bfd)
   if (data == NULL)
     {
       if (bfd_get_error () == bfd_error_no_error)
-	return NULL;
+	{
+	  if (!require)
+	    return nullptr;
+	  error (_("could not read '.gnu_debugaltlink' section"));
+	}
       error (_("could not read '.gnu_debugaltlink' section: %s"),
 	     bfd_errmsg (bfd_get_error ()));
     }
@@ -6300,7 +6304,7 @@ get_abbrev_section_for_cu (struct dwarf2_per_cu_data *this_cu)
   dwarf2_per_bfd *per_bfd = this_cu->per_bfd;
 
   if (this_cu->is_dwz)
-    abbrev = &dwarf2_get_dwz_file (per_bfd)->abbrev;
+    abbrev = &dwarf2_get_dwz_file (per_bfd, true)->abbrev;
   else
     abbrev = &per_bfd->abbrev;
 
@@ -20515,7 +20519,7 @@ read_attribute_value (const struct die_reader_specs *reader,
       /* FALLTHROUGH */
     case DW_FORM_GNU_strp_alt:
       {
-	dwz_file *dwz = dwarf2_get_dwz_file (per_objfile->per_bfd);
+	dwz_file *dwz = dwarf2_get_dwz_file (per_objfile->per_bfd, true);
 	LONGEST str_offset = cu_header->read_offset (abfd, info_ptr,
 						     &bytes_read);
 
@@ -21113,7 +21117,7 @@ get_debug_line_section (struct dwarf2_cu *cu)
     section = &cu->dwo_unit->dwo_file->sections.line;
   else if (cu->per_cu->is_dwz)
     {
-      dwz_file *dwz = dwarf2_get_dwz_file (per_objfile->per_bfd);
+      dwz_file *dwz = dwarf2_get_dwz_file (per_objfile->per_bfd, true);
 
       section = &dwz->line;
     }
