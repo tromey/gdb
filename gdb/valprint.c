2793ed94d9aa4fd8af59ed31fd041490b1e3ee92
Tom Tromey <tom@tromey.com>
2022-02-12 18:41:34 -0700

    Fix a latent bug in print_wchar
    print_wchar keeps track of when escape sequences are emitted, to force
    an escape sequence if needed by a subsequent character.  For example
    for the string concatenation "\0" "1", gdb will print "\000\061" --
    because printing "\0001" might be confusing.
    
    However, this code has two errors.  First, this logic is not needed
    for octal escapes, because there is a length limit of 3 for octal
    escapes, and gdb always prints these with "%.3o".  Second, though,
    this *is* needed for hex escapes, because those do not have a length
    limit.
    
    This patch fixes these problems and adds the appropriate tests.

--- gdb/valprint.c
+++ gdb/valprint.c
@@ -2058,9 +2058,7 @@ print_wchar (gdb_wint_t w, const gdb_byte *orig,
 	break;
       default:
 	{
-	  if (gdb_iswprint (w) && (!need_escape || (!gdb_iswdigit (w)
-						    && w != LCST ('8')
-						    && w != LCST ('9'))))
+	  if (gdb_iswprint (w) && !(need_escape && gdb_iswxdigit (w)))
 	    {
 	      gdb_wchar_t wchar = w;
 
@@ -2082,10 +2080,19 @@ print_wchar (gdb_wint_t w, const gdb_byte *orig,
 		  /* If the value fits in 3 octal digits, print it that
 		     way.  Otherwise, print it as a hex escape.  */
 		  if (value <= 0777)
-		    xsnprintf (octal, sizeof (octal), "\\%.3o",
-			       (int) (value & 0777));
+		    {
+		      xsnprintf (octal, sizeof (octal), "\\%.3o",
+				 (int) (value & 0777));
+		      *need_escapep = false;
+		    }
 		  else
-		    xsnprintf (octal, sizeof (octal), "\\x%lx", (long) value);
+		    {
+		      xsnprintf (octal, sizeof (octal), "\\x%lx", (long) value);
+		      /* A hex escape might require the next character
+			 to be escaped, because, unlike with octal,
+			 hex escapes have no length limit.  */
+		      *need_escapep = true;
+		    }
 		  append_string_as_wide (octal, output);
 		}
 	      /* If we somehow have extra bytes, print them now.  */
@@ -2094,11 +2101,10 @@ print_wchar (gdb_wint_t w, const gdb_byte *orig,
 		  char octal[5];
 
 		  xsnprintf (octal, sizeof (octal), "\\%.3o", orig[i] & 0xff);
+		  *need_escapep = false;
 		  append_string_as_wide (octal, output);
 		  ++i;
 		}
-
-	      *need_escapep = true;
 	    }
 	  break;
 	}
