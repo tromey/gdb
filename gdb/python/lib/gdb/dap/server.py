5d33cb5d4bfefd5458b4268fe3fbc0eaa9dfc6b6
Tom Tromey <tromey@adacore.com>
2025-12-16 08:43:43 -0700

    Fix DAP 'disconnect' implementation
    gdb's implementation of the DAP 'disconnect' request was incorrect in
    a few ways.
    
    First, the 'terminateDebuggee' field is optional, and has a special
    meaning when not supplied: it should do whatever the default is.
    
    Second, if the inferior was attached, it should detach rather than
    terminate by default.
    
    Finally, if the inferior was not started at all, it seems reasonable
    for this request to simply succeed silently -- currently it returns
    "success: false" with the reason being that the inferior isn't
    running.
    
    Approved-By: Andrew Burgess <aburgess@redhat.com>

--- gdb/python/lib/gdb/dap/server.py
+++ gdb/python/lib/gdb/dap/server.py
@@ -19,6 +19,7 @@ import inspect
 import json
 import threading
 from contextlib import contextmanager
+from typing import Optional
 
 import gdb
 
@@ -618,11 +619,26 @@ def terminate(**args):
     exec_and_log("kill")
 
 
+@in_gdb_thread
+def _disconnect_or_kill(terminate: Optional[bool]):
+    inf = gdb.selected_inferior()
+    if inf.connection is None:
+        # Nothing to do here.
+        return
+    if terminate is None:
+        # The default depends on whether the inferior was attached or
+        # launched.
+        terminate = not inf.was_attached
+    if terminate:
+        exec_and_log("kill")
+    elif inf.was_attached:
+        exec_and_log("detach")
+
+
 @request("disconnect", on_dap_thread=True, expect_stopped=False)
 @capability("supportTerminateDebuggee")
-def disconnect(*, terminateDebuggee: bool = False, **args):
-    if terminateDebuggee:
-        send_gdb_with_response(lambda: exec_and_log("kill"))
+def disconnect(*, terminateDebuggee: Optional[bool] = None, **args):
+    send_gdb_with_response(lambda: _disconnect_or_kill(terminateDebuggee))
     _server.shutdown()
 
 
