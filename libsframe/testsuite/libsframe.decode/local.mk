55575b6058fc228609e9a56c7860976f3e02b0ee
Indu Bhagat <indu.bhagat@oracle.com>
2026-01-15 16:44:03 -0800

    [SFrame-V3] libsframe: testsuite: add new testcases for SFRAME_FDE_TYPE_FLEX
    This patch adds two new tests for SFrame V3 changes, focusing on the
    newly added flexible FDE TYPE SFRAME_FDE_TYPE_FLEX.
    
    Following tests are added:
      - be-flipping-v3.c: Validates that big-endian SFrame V3 data is
        correctly endian flipped when run on little-endian hosts.  It
        verifies the decoding of CFA offsets and the new V3 register/offset
        metadata bitfields using the SFRAME_V3_FLEX_FDE_OFFSET_REG_* macros.
      - findfre-flex-1.c: Tests a variety of sframe_find_fre lookup
        scenarios, apart from checking the basic encoder/decoder APIs.
    
    Documentation for the binary test data DATA-BE-V3 is provided in
    README-be-flipping-v3 to ensure reproducibility.
    
    libsframe/
    	* Makefile.in: Regenerate.
    	* testsuite/libsframe.decode/DATA-BE-V3: New test data.
    	* testsuite/libsframe.decode/README-be-flipping-v3: New file.
    	* testsuite/libsframe.decode/be-flipping-v3.c: New test.
    	* testsuite/libsframe.decode/decode.exp: Run be-flipping-v3.
    	* testsuite/libsframe.decode/local.mk: Add be-flipping-v3.
    	* testsuite/libsframe.find/find.exp: Run findfre-flex-1.
    	* testsuite/libsframe.find/findfre-flex-1.c: New test.
    	* testsuite/libsframe.find/local.mk: Add findfre-flex-1.

--- libsframe/testsuite/libsframe.decode/local.mk
+++ libsframe/testsuite/libsframe.decode/local.mk
@@ -1,11 +1,16 @@
 check_PROGRAMS =
 if HAVE_COMPAT_DEJAGNU
-  check_PROGRAMS += %D%/be-flipping %D%/frecnt-1 %D%/frecnt-2 %D%/frecnt-v2
+  check_PROGRAMS += %D%/be-flipping %D%/be-flipping-v3 \
+                    %D%/frecnt-1 %D%/frecnt-2 %D%/frecnt-v2
 endif
 
 %C%_be_flipping_SOURCES = %D%/be-flipping.c
 %C%_be_flipping_LDADD = $(testsuite_LDADD)
 %C%_be_flipping_CPPFLAGS = $(testsuite_CPPFLAGS)
+
+%C%_be_flipping_v3_SOURCES = %D%/be-flipping-v3.c
+%C%_be_flipping_v3_LDADD = $(testsuite_LDADD)
+%C%_be_flipping_v3_CPPFLAGS = $(testsuite_CPPFLAGS)
 
 %C%_frecnt_1_SOURCES = %D%/frecnt-1.c
 %C%_frecnt_1_LDADD = $(testsuite_LDADD)
