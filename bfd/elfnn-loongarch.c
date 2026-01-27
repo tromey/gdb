6f8d261d200b59f0cc55581ad6cefe3912a05fa9
Alan Modra <amodra@gmail.com>
2026-01-28 09:48:41 +1030

    Re: PR 33840 typo in elfnn-loongarch.c
    The second string with the typo was unused (and had unescaped %).
    Remove it.

--- bfd/elfnn-loongarch.c
+++ bfd/elfnn-loongarch.c
@@ -3481,11 +3481,7 @@ loongarch_resolve_pcrel_lo_relocs (loongarch_pcrel_relocs *p)
       loongarch_pcrel_hi_reloc search = {r->address, 0, 0, NULL};
       loongarch_pcrel_hi_reloc *entry = htab_find (p->hi_relocs, &search);
 
-      char *string = NULL;
       if (entry == NULL)
-	string = _("%pcrel_lo missing matching %pcrel_hi");
-
-      if (string != NULL)
 	{
 	  ((*r->info->callbacks->reloc_overflow)
 	   (r->info, NULL, NULL, r->howto->name, (bfd_vma) 0,
